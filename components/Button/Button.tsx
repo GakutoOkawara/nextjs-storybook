00e2a8bfd105dfc0d79eeb28f948682f0aa7f2af
GakutoOkawara <ookawara0817@gmail.com>
2022-12-28 00:12:19 +0900

    add Button.stories and remove unnecessary stories

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,17 +3,15 @@ import styles from "./Button.module.css"
 type Button = {
   size?: string
   variant?: string
-  loading?: boolean
   fullWidth?: boolean
   children?: string
 }
 
-export const Button = ({size, variant, loading, fullWidth, children}: Button) => {
+export const Button = ({size, variant, fullWidth, children}: Button) => {
   const classNameList = [styles.button]
 
   if(size) classNameList.push(styles[`size-${size}`])
   if(variant) classNameList.push(styles[`variant-${variant}`])
-  if(loading) classNameList.push(styles["loading"])
   if(fullWidth) classNameList.push(styles["full-width"])
 
   const classNames = classNameList.join(" ")
